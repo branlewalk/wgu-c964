e380ca73af46ef6e8dc83a70bc358c1da9a9019a
branlewalk <branlewalk@gmail.com>
2020-12-04 12:31:01 -0800

    added login and documentation, edited visualizations.

--- wine_app.py
+++ wine_app.py
@@ -1,7 +1,8 @@
 import flask
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from joblib import load
+from functools import wraps
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -9,9 +10,44 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 user_input = None
 
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'WineCo' or request.form['password'] != 'Red&White':
+            error = 'Wrong username or password. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
+
 @app.route("/")
+@login_required
 def index():
     return flask.render_template("index.html")
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return flask.render_template("dashboard.html")
 
 
 def value_predictor(wine_type, to_predict_list):
@@ -25,6 +61,7 @@ def value_predictor(wine_type, to_predict_list):
 
 
 @app.route("/predict", methods=["POST"])
+@login_required
 def result():
     if request.method == "POST":
         to_predict_list = request.form.to_dict()
@@ -45,14 +82,16 @@ def result():
 
 
 @app.route("/predict")
+@login_required
 def predict():
     return render_template("predict.html")
 
 
 @app.route("/about")
+@login_required
 def about():
     return render_template("about.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
